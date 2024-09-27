1a59ca55cd6bf7507f046b0e1ed645226640b548
xuzhenggang <xuzhenggang@sofish.com>
2024-09-27 09:53:58 +0800

    123e

--- my-app-ele/src/index.js
+++ my-app-ele/src/index.js
@@ -22,11 +22,11 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  // mainWindow.loadFile(path.join(__dirname, "index.html"));
-  mainWindow.loadURL("https://cms-manager.sofishgame.com/index");
+  mainWindow.loadFile(path.join(__dirname, "pages/index.html"));
+  // mainWindow.loadURL("https://cms-manager.sofishgame.com/index");
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
